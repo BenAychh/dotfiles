4eb53fe48aefaa25d9dc9a043913f0c0711ee53e
Ben Hernandez <benaychh@gmail.com>
2018-10-27 17:24:53 -0500

    Updating install scripts

--- init/20_zsh.sh
+++ init/20_zsh.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)";
+curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh;
 
 git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k;
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting;
